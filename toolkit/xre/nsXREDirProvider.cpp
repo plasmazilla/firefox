stephenfox/iceweasel-dev

Add another preferences directory for applications: preferences/syspref
It was existing in previous versions of iceweasel as a symlink to
/etc/iceweasel/pref.

This has the side effect to make these preferences there work again, and
to disable the "set as default browser" dialog.

toolkit/xre/nsXREDirProvider.cpp
@@ -614,6 +614,7 @@ nsXREDirProvider::LoadAppBundleDirs()
 }
 
 static const char *const kAppendPrefDir[] = { "defaults", "preferences", nsnull };
+static const char *const kAppendSysPrefDir[] = { "defaults", "syspref", nsnull };
 
 #ifdef DEBUG_bsmedberg
 static void
@@ -670,6 +671,7 @@ nsXREDirProvider::GetFilesInternal(const char* aProperty,
     LoadAppDirIntoArray(mXULAppDir, kAppendPrefDir, directories);
     LoadDirsIntoArray(mAppBundleDirectories,
                       kAppendPrefDir, directories);
+    LoadAppDirIntoArray(mXULAppDir, kAppendSysPrefDir, directories);
 
     rv = NS_NewArrayEnumerator(aResult, directories);
   }

plasmazilla/firefox

toolkit/xre/nsXREDirProvider.cpp
@@ -989,6 +989,7 @@ nsXREDirProvider::LoadAppBundleDirs()
 #endif
 
 static const char *const kAppendPrefDir[] = { "defaults", "preferences", nullptr };
+static const char *const kAppendSysPrefDir[] = { "defaults", "syspref", nullptr };
 
 #ifdef DEBUG_bsmedberg
 static void
@@ -1030,6 +1031,7 @@ nsXREDirProvider::GetFilesInternal(const char* aProperty,
     LoadDirIntoArray(mXULAppDir, kAppendPrefDir, directories);
     LoadDirsIntoArray(mAppBundleDirectories,
                       kAppendPrefDir, directories);
+    LoadDirIntoArray(mXULAppDir, kAppendSysPrefDir, directories);
 
     rv = NS_NewArrayEnumerator(aResult, directories);
   }